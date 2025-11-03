0dda76c8759afbd30dd9488e8afade1326079825
antoniolocascio <antonio.locascio1@gmail.com>
2025-11-03 14:21:38 +0800

    Perform block gas/blob gas limit check for tester

--- basic_bootloader/src/bootloader/mod.rs
+++ basic_bootloader/src/bootloader/mod.rs
@@ -374,41 +374,41 @@ where
         logs_used: u64,
         blob_gas_used: u64,
     ) -> Result<(), InvalidTransaction> {
-        if cfg!(feature = "resources_for_tester") {
-            // EVM tester uses some really high gas limits,
-            // so we don't limit the block's native resource.
-            Ok(())
+        let mut logger = system.get_logger();
+        if gas_used > system.get_gas_limit() {
+            let _ = logger.write_fmt(format_args!(
+                "Block gas limit reached, invalidating transaction\n"
+            ));
+            Err(InvalidTransaction::BlockGasLimitReached)
+        } else if blob_gas_used > system.get_blob_gas_limit() {
+            let _ = logger.write_fmt(format_args!(
+                "Block blob gas limit reached, invalidating transaction\n"
+            ));
+            Err(InvalidTransaction::BlockBlobGasLimitReached)
+        } else if !cfg!(feature = "resources_for_tester")
+            && computational_native_used > MAX_NATIVE_COMPUTATIONAL
+        {
+            // ZKsync OS-specific resources are not checked for evm tester
+            let _ = logger.write_fmt(format_args!(
+                "Block native limit reached, invalidating transaction\n"
+            ));
+            Err(InvalidTransaction::BlockNativeLimitReached)
+        } else if !cfg!(feature = "resources_for_tester")
+            && pubdata_used > system.get_pubdata_limit()
+        {
+            // ZKsync OS-specific resources are not checked for evm tester
+            let _ = logger.write_fmt(format_args!(
+                "Block pubdata limit reached, invalidating transaction\n"
+            ));
+            Err(InvalidTransaction::BlockPubdataLimitReached)
+        } else if !cfg!(feature = "resources_for_tester") && logs_used > MAX_NUMBER_OF_LOGS {
+            // ZKsync OS-specific resources are not checked for evm tester
+            let _ = logger.write_fmt(format_args!(
+                "Block logs limit reached, invalidating transaction\n"
+            ));
+            Err(InvalidTransaction::BlockL2ToL1LogsLimitReached)
         } else {
-            let mut logger = system.get_logger();
-
-            if gas_used > system.get_gas_limit() {
-                let _ = logger.write_fmt(format_args!(
-                    "Block gas limit reached, invalidating transaction\n"
-                ));
-                Err(InvalidTransaction::BlockGasLimitReached)
-            } else if computational_native_used > MAX_NATIVE_COMPUTATIONAL {
-                let _ = logger.write_fmt(format_args!(
-                    "Block native limit reached, invalidating transaction\n"
-                ));
-                Err(InvalidTransaction::BlockNativeLimitReached)
-            } else if pubdata_used > system.get_pubdata_limit() {
-                let _ = logger.write_fmt(format_args!(
-                    "Block pubdata limit reached, invalidating transaction\n"
-                ));
-                Err(InvalidTransaction::BlockPubdataLimitReached)
-            } else if logs_used > MAX_NUMBER_OF_LOGS {
-                let _ = logger.write_fmt(format_args!(
-                    "Block logs limit reached, invalidating transaction\n"
-                ));
-                Err(InvalidTransaction::BlockL2ToL1LogsLimitReached)
-            } else if blob_gas_used > system.get_blob_gas_limit() {
-                let _ = logger.write_fmt(format_args!(
-                    "Block blob gas limit reached, invalidating transaction\n"
-                ));
-                Err(InvalidTransaction::BlockBlobGasLimitReached)
-            } else {
-                Ok(())
-            }
+            Ok(())
         }
     }
 }
